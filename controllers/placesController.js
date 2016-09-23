4ce6683804ff4bdb04b94a65e4b9c3fdde4fad9a
laurakathleen <laurakwalley@gmail.com>
2016-09-23 10:01:06 -0700

    cleans up comments

--- controllers/placesController.js
+++ controllers/placesController.js
@@ -7,13 +7,6 @@ PlacesController.$inject = ['PlaceService', '$routeParams', '$location'];
 function PlacesController(PlaceService, $routeParams, $location){
 	var vm = this;
 
-	// var mountain = document.getElementById('mountain');
-	// var desert = getElementById('desert');
-	// var beach = getElementById('beach');
-	// var plains = getElementById('plains');
-	// var camping = document.getElementById('camping');
-	// var city = getElementById('city');
-
 	// vm.getPlaces = function(){
 	// 	if (angular.element('#mountain').hasClass('clicked') && angular.element('#camping').hasClass('clicked')){
 	// 		console.log(places.mountainCamping);
@@ -26,27 +19,6 @@ function PlacesController(PlaceService, $routeParams, $location){
 	// 		return results;
 	// 		console.log(results);
 	// 	}
-	// }
-
-	// vm.getPlaces = function(){
-	// 	if (mountain.hasClass('clicked') && camping.hasClass('clicked')){
-	// 		var results = places.mountainCamping;
-	// 	} else if (mountain == true && city == true){
-	// 		results = places.mountainCities;
-	// 	} else if (desert == true && camping == true){
-	// 		results = places.desertCamping;
-	// 	} else if (desert == true && city == true){
-	// 		results = places.desertCities;
-	// 	} else if (beach == true && camping == true){
-	// 		results = places.beachCamping;
-	// 	} else if (beach == true && city == true){
-	// 		results = places.beachCities;
-	// 	} else if (plains == true && camping == true){
-	// 		results = places.plainsCamping;
-	// 	} else {
-	// 		results = places.plainsCities;
-	// 	}
-	// 	return results;
 	// }
 
 	//use this one:
