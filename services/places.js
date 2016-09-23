a34ea23658f676d77b03cb47fd93647c36566967
laurakathleen <laurakwalley@gmail.com>
2016-09-23 10:00:09 -0700

    cleans up code a bit

--- services/places.js
+++ services/places.js
@@ -6,62 +6,6 @@ angular.module('roadTripIt')
 		console.log('inside place service');
 		var self = this;
 
-		// self.places = [
-		// 	{type: 'mountainCamping', allPlaces: ['Estes Park, Colorado', 'Acadia National Park, Maine', 'White Mountain National Forest, New Hampshire', 'Green Mountain National Forest, Vermont', 'Shenandoah National Park, Virginia', 'Minnewaska State Park Preserve, New York', 'Pine Grove Furnace State Park, Pennsylvania', 'Badlands National Park, South Dakota', 'Denali National Park, Alaska', 'Glacier Bay National Park, Alaska', 'Yosemite National Park, California', 'Sequoia and Kings Canyon National Parks, California', 'Olympic National Park, Washington', 'Gunnison National Forest, Colorado', 'Glacier National Park, Montana', 'Grand Teton National Park, Wyoming', 'Capitol Reef National Park, Utah', 'Sawtooth National Forest, Idaho', 'Great Smoky Mountains National Park, Tennessee']},
-		// 	{type: 'beachCamping', allPlaces: ['Assateague Island National Seashore, Maryland', 'Jalama Beach County Park, California', 'Pfeiffer Big Sur State Park, California', 'Long Key State Park, Florida', 'Myrtle Beach Travel Park, South Carolina', 'Padre Island National Seashore, Texas', 'Little Tybee Island, Georgia', 'Olympic National Park, Washington', 'Padre Island National Seashore, Texas', "Wai'anapanapa State Park, Hawaii", 'Hammocks Beach State Park, North Carolina', 'Horseneck Beach State Reservation, Massachusetts', 'Anastasia State Park, Florida', 'Siuslaw National Forest, Oregon', 'Ninilchik State Recreation Area, Alaska']},
-		// 	{type: 'desertCamping', allPlaces: ['Joshua Tree National Park, California', 'Death Valley National Park, California', 'Moab, Utah', 'Lava Beds National Monument, California', 'Mojave National Preserve, California', 'Valley of Fire State Park, Nevada', 'Owens Valley, California', 'Cathedral Gorge State Park, Nevada', 'Mojave National Preserve, California', 'Black Rock Desert Recreation Area, Nevada', 'Lost Dutchman State Park, Arizona', 'Ute Lake State Park, New Mexico', 'Abiquiu Lake, New Mexico']},
-		// 	{type: 'mountainCities', allPlaces: ['Denver, Colorado', 'Reno, Nevada', 'Boulder, Colorado', ],
-		// 	'desertCities': ['Flagstaff, Arizona', 'Palm Springs, California', 'Phoenix, Arizona', 'Las Vegas, Nevada', 'Santa Fe, New Mexico', 'Scottsdale, Arizona', 'Tucson, Arizona']},
-		// 	{type: 'beachCities', allPlaces: ['Miami, Florida', 'San Diego, California', 'Malibu, California', 'Santa Cruz, California', 'New York City, New York', 'San Francisco, California', 'Traverse City, Michigan', 'Key West, Florida', 'Chincoteague, Virginia', 'Newport, Rhode Island', 'York Beach, Maine', 'Cape May, New Jersey']},
-		// 	{type: 'plainCamping', allPlaces: ['Sage Creek Campground, South Dakota']},
-		// 	{type: 'plainCities', allPlaces: ['Rapid City, South Dakota']}
-
-		// ];
-
-
-		// self.places = [
-		// 		{ 
-		// 		name: 'San Diego',
-		// 		type: 'beach',
-		// 		field: 'city'
-		// 	},
-		// 	{
-		// 		name: 'Francis Bay Campsite',
-		// 		type: 'beach',
-		// 		field: 'camping',
-		// 	},
-		// 	{
-		// 		name: 'Denver',
-		// 		type: 'mountain',
-		// 		field: 'city'
-		// 	},
-		// 	{
-		// 		name: 'Tahoe',
-		// 		type: 'mountain',
-		// 		field: 'camping'
-		// 	},
-		// 	{
-		// 		name: 'Palm Springs',
-		// 		type: 'desert',
-		// 		field: 'city'
-		// 	},
-		// 	{
-		// 		name: 'Mesquite Springs Campground',
-		// 		type: 'desert',
-		// 		field: 'camping',
-		// 	},
-		// 	{
-		// 		name: 'Omaha',
-		// 		type: 'plain',
-		// 		field: 'city'
-		// 	},
-		// 	{
-		// 		name: 'Lake MacBride State Park',
-		// 		type:'plain',
-		// 		field: 'camping'
-		// 	}
-		// ];
-
 
 		self.mountainCamping = ['Estes Park, Colorado', 'Acadia National Park, Maine', 'White Mountain National Forest, New Hampshire', 'Green Mountain National Forest, Vermont', 'Shenandoah National Park, Virginia', 'Minnewaska State Park Preserve, New York', 'Pine Grove Furnace State Park, Pennsylvania', 'Badlands National Park, South Dakota', 'Denali National Park, Alaska', 'Glacier Bay National Park, Alaska', 'Yosemite National Park, California', 'Sequoia and Kings Canyon National Parks, California', 'Olympic National Park, Washington', 'Gunnison National Forest, Colorado', 'Glacier National Park, Montana', 'Grand Teton National Park, Wyoming', 'Capitol Reef National Park, Utah', 'Sawtooth National Forest, Idaho', 'Great Smoky Mountains National Park, Tennessee'];
 		self.beachCamping = ['Assateague Island National Seashore, Maryland', 'Jalama Beach County Park, California', 'Pfeiffer Big Sur State Park, California', 'Long Key State Park, Florida', 'Myrtle Beach Travel Park, South Carolina', 'Padre Island National Seashore, Texas', 'Little Tybee Island, Georgia', 'Olympic National Park, Washington', 'Padre Island National Seashore, Texas', "Wai'anapanapa State Park, Hawaii", 'Hammocks Beach State Park, North Carolina', 'Horseneck Beach State Reservation, Massachusetts', 'Anastasia State Park, Florida', 'Siuslaw National Forest, Oregon', 'Ninilchik State Recreation Area, Alaska'],
@@ -120,7 +64,7 @@ angular.module('roadTripIt')
 			console.log('printing all places');
 		};
 
-		// //return mountainCamping places:
+		//return mountainCamping places:
 		// self.queryMtnCamp = function() {
 		// 	return self.mountainCamping;
 		// 	console.log("printing mountain camping");
